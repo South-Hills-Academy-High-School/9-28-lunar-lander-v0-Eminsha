db1e41590a1ff6b8d54f56815dbd01aedc510a25
Eminsha <113388072+Eminsha@users.noreply.github.com>
2022-09-29 13:49:43 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,20 +2,34 @@ namespace SpriteKind {
     export const map = SpriteKind.create()
     export const rocketengine = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.ay = -25
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    apple.ay = 25 * Math.sin(angle)
+    apple.ax = 25 * Math.cos(angle)
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    apple.ay = 20
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    angle += 30 * (3.14 / 180)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, function (sprite, location) {
-    mySprite.setVelocity(0, -1)
+    apple.setVelocity(0, -1)
 })
-controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    mySprite.ay = 20
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    angle += -30 * (3.14 / 180)
 })
-let mySprite: Sprite = null
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    apple.ay = 20
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    apple.ay = 60
+})
+let apple: Sprite = null
 let angle = 0
+angle = 0
 tiles.setCurrentTilemap(tilemap`level1`)
 effects.clouds.startScreenEffect()
-mySprite = sprites.create(img`
+apple = sprites.create(img`
     . . . . . . . e c 7 . . . . . . 
     . . . . e e e c 7 7 e e . . . . 
     . . c e e e e c 7 e 2 2 e e . . 
@@ -33,26 +47,20 @@ mySprite = sprites.create(img`
     . . . 2 2 e e 4 4 4 2 e e . . . 
     . . . . . 2 2 e e e e . . . . . 
     `, SpriteKind.Player)
-let mySprite3 = sprites.create(img`
-    7 3 
-    3 7 
-    7 3 
-    3 7 
-    7 3 
-    3 7 
-    7 3 
-    3 7 
+let engine = sprites.create(img`
+    7 3 7 3 . . . . 
+    3 7 3 7 . . . . 
     `, SpriteKind.rocketengine)
-scene.cameraFollowSprite(mySprite)
-scaling.scaleByPercent(mySprite, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-mySprite.ay = 20
+scene.cameraFollowSprite(apple)
+scaling.scaleByPercent(apple, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+apple.ay = 20
 let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
-let mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
+let minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
 game.onUpdate(function () {
-    mySprite2.destroy()
+    minimap2.destroy()
     myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
-    minimap.includeSprite(myMinimap, mySprite, MinimapSpriteScale.MinimapScale)
-    mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
-    mySprite2.setPosition(mySprite.left, mySprite.top)
-    mySprite3.setPosition(mySprite.x, mySprite.y)
+    minimap.includeSprite(myMinimap, apple, MinimapSpriteScale.MinimapScale)
+    minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
+    minimap2.setPosition(apple.x - 50, apple.y - 30)
+    engine.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
 })
