4a7235f31f3a6f47dbf979107f90ef1222ef7c31
Eminsha <113388072+Eminsha@users.noreply.github.com>
2022-09-29 14:19:54 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ namespace SpriteKind {
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     apple.ay = 25 * Math.sin(angle)
     apple.ax = 25 * Math.cos(angle)
+    fireball.setFlag(SpriteFlag.Invisible, false)
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     apple.ay = 20
@@ -20,36 +21,71 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
     apple.ay = 20
+    fireball.setFlag(SpriteFlag.Invisible, true)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     apple.ay = 60
 })
 let apple: Sprite = null
 let angle = 0
+let fireball: Sprite = null
+fireball = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 4 4 4 4 4 . . . . . . 
+    . . . 4 4 4 5 5 5 d 4 4 4 4 . . 
+    . . 4 d 5 d 5 5 5 d d d 4 4 . . 
+    . . 4 5 5 1 1 1 d d 5 5 5 4 . . 
+    . 4 5 5 5 1 1 1 5 1 1 5 5 4 4 . 
+    . 4 d d 1 1 5 5 5 1 1 5 5 d 4 . 
+    . 4 5 5 1 1 5 1 1 5 5 d d d 4 . 
+    . 2 5 5 5 d 1 1 1 5 1 1 5 5 2 . 
+    . 2 d 5 5 d 1 1 1 5 1 1 5 5 2 . 
+    . . 2 4 d d 5 5 5 5 d d 5 4 . . 
+    . . . 2 2 4 d 5 5 d d 4 4 . . . 
+    . . 2 2 2 2 2 4 4 4 2 2 2 . . . 
+    . . . 2 2 4 4 4 4 4 4 2 2 . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    `, SpriteKind.Player)
+fireball.setFlag(SpriteFlag.Invisible, true)
 angle = 0
 tiles.setCurrentTilemap(tilemap`level1`)
 effects.clouds.startScreenEffect()
 apple = sprites.create(img`
-    . . . . . . . e c 7 . . . . . . 
-    . . . . e e e c 7 7 e e . . . . 
-    . . c e e e e c 7 e 2 2 e e . . 
-    . c e e e e e c 6 e e 2 2 2 e . 
-    . c e e e 2 e c c 2 4 5 4 2 e . 
-    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
-    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
-    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
-    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
-    . . . 2 2 e e 4 4 4 2 e e . . . 
-    . . . . . 2 2 e e e e . . . . . 
+    . . . . . . 2 2 2 2 2 2 . . . . 
+    . . . . . 2 3 3 3 3 3 3 2 . . . 
+    . . . . . 2 3 1 1 1 1 3 2 . . . 
+    . . . . . 3 2 1 1 1 1 2 3 . . . 
+    . . . . . 3 2 1 1 1 1 2 3 . . . 
+    . . . . . 3 2 1 1 1 1 2 3 . . . 
+    . . . . . 3 2 1 1 1 1 2 3 . . . 
+    . . . . . 3 2 1 1 1 1 2 3 . . . 
+    . . . . . 2 3 1 1 1 1 2 3 . . . 
+    . . . . . 2 3 1 1 1 1 3 2 . . . 
+    . . . . . 2 3 1 1 1 1 3 2 . . . 
+    . . . . . 2 2 1 1 1 1 2 2 . . . 
+    . . . . . 2 2 3 1 1 2 3 2 . . . 
+    . . . . . . 3 2 1 1 2 3 . . . . 
+    . . . . . . 3 2 1 1 2 3 . . . . 
+    . . . . . . 3 2 2 2 2 3 . . . . 
     `, SpriteKind.Player)
 let engine = sprites.create(img`
-    7 3 7 3 . . . . 
-    3 7 3 7 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 5 5 5 5 5 . . . . . 
+    . . . . . . 5 5 5 5 5 . . . . . 
+    . . . . . . 4 5 5 5 4 4 . . . . 
+    . . . . . . 4 5 5 5 4 4 . . . . 
+    . . . . . . 4 4 5 5 4 . . . . . 
+    . . . . . . 4 4 4 4 4 . . . . . 
+    . . . . . . . . 4 4 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.rocketengine)
 scene.cameraFollowSprite(apple)
 scaling.scaleByPercent(apple, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
@@ -63,4 +99,5 @@ game.onUpdate(function () {
     minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
     minimap2.setPosition(apple.x - 50, apple.y - 30)
     engine.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
+    fireball.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
 })
